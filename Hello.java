2a05ebb5de6cde765933eeb7738475d382401799
Nancy <nancyahmedmoh@gmail.com>
2024-07-10 12:49:39 +0300

    ok2

--- Hello.java
+++ Hello.java
@@ -2,5 +2,10 @@ public class Hello {
     public static void main(String[] args) {
         // Prints the classic "Hello, World!" message
         System.out.println("Hello, World!");
+        greet("Student");
+    }
+
+    public static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
     }
 }
